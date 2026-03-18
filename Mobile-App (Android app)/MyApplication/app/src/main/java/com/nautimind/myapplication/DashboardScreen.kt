aeef5448c92453f52754508b587336dd71cd055b
aishwaryguptadz <aishaakash182@gmail.com>
2026-03-18 11:37:04 +0530

    TEAM_DIAMOND recovered

---
+++ Mobile-App (Android app)/MyApplication/app/src/main/java/com/nautimind/myapplication/DashboardScreen.kt
@@ -0,0 +1,14 @@
+package com.nautimind.myapplication
+
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+
+@Composable
+fun DashboardScreen(modifier: Modifier = Modifier) {
+
+    Text(
+        text = "Dashboard",
+        modifier = modifier
+    )
+}
---
+++ Mobile-App (Android app)/MyApplication/app/src/main/java/com/nautimind/myapplication/MetricCard.kt
@@ -0,0 +1,4 @@
+package com.nautimind.myapplication
+
+class MetricCard {
+}
